48911bb79f31d8bb699664e8b9ad54e4f39f0a52
Rizki Afandi <nama.rizki.afandi@gmail.com>
2022-03-24 18:57:14 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+// Create database connection using config file
+include_once("config.php");
+ 
+// Fetch all users data from database
+$result = mysqli_query($mysqli, "SELECT * FROM id");
+?>
+ 
+<html>
+<head>    
+    <title>Homepage</title>
+</head>
+ 
+<body>
+<a href="add.php">Add New User</a><br/><br/>
+ 
+    <table width='80%' border=1>
+ 
+    <tr>
+        <th>No</th>
+        <th>id</th>
+        <th>nim</th>
+        <th>Name</th>
+        <th>Mobile</th>
+        <th>Email</th>
+        <th>Update</th>
+    </tr>
+    <?php  
+    while($user_data = mysqli_fetch_array($result)) {    
+        $no = 1;     
+        echo "<tr>";
+        echo "<td>".$user_data['id']."</td>";
+        echo "<td>".$user_data['nim']."</td>";
+        echo "<td>".$user_data['nama']."</td>";   
+        echo "<td>".$user_data['email']."</td>"; 
+        echo "<td>".$user_data['mobile']."</td>";  
+        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
+    }
+    ?>
+    </table>
+</body>
+</html>
